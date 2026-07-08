77ef58c24fa9b016503ad2ec0a2a885d1172fc9d
Antoan Sarafski <98612405+AntoanSarafski@users.noreply.github.com>
2026-07-08 19:58:35 +0300

    Implement game loop and falling pieces

--- Tetris/Program.cs
+++ Tetris/Program.cs
@@ -1,4 +1,6 @@
-﻿class Program
+﻿using System.Transactions;
+
+class Program
 {
     // Settings for the Tetris game
 
@@ -7,9 +9,52 @@
     static int InfoCols = 10;
     static int ConsoleRows = 1 + TetrisRows + 1;
     static int ConsoleCols = 1 + TetrisCols + 1 + InfoCols + 1;
+    static List<bool[,]> TetrisFigures = new List<bool[,]>()
+        {
+            // L
+            new bool[,] { {true, true, true, true} },  // I
+            new bool[,]                                // O
+                {
+                    { true, true },
+                    { true, true }
+                },
+            new bool[,]                                // T
+                {
+                    { false, true, false },
+                    { true, true, true }
+                },
+            new bool[,]                               // S
+                {
+                    { false, true, true },
+                    { true, true, false }
+                },
+
+            new bool[,]
+                {                                     // Z
+                    { true, true, false },
+                    { false, true, true }
+                },
+            new bool[,]
+                {                                     // J
+                    { true, false, false },
+                    { true, true, true }
+                },
+            new bool[,]                               // L
+                {
+                    {false, false, true },
+                    { true, true, true }
+                }
+        };
+
 
     // State
     static int Score = 0;
+    static int Frame = 0;
+    static int FramesToMoveFigure = 15;
+    static int CurrentFigureIndex = 2;  // TODO: Must be random
+    static int CurrentFigureRow = 0;
+    static int CurrentFigureCol = 0;
+    static bool[,] TetrisField = new bool[TetrisRows, TetrisCols];
 
 
     static void Main(string[] args)
@@ -24,66 +69,141 @@
         Console.CursorVisible = false;
         DrawBorder();
         DrawInfo();
-        Console.ReadLine();
-    }
-
-
-
-    static void DrawBorder()
-    {
-        string line = "╔";
-        line += new string('═', TetrisCols);
-
-        /*for (int i = 0; i < TetrisCols; i++)
+        while (true)
         {
-            line += "═";
-        }*/
 
-        line += "╦";
-        line += new string('═', InfoCols);
-        line += "╗";
 
-        Console.Write(line);
+            if (Console.KeyAvailable)
+            {
+                var key = Console.ReadKey(true);
+                if (key.Key == ConsoleKey.Escape)
+                {
+                    return;
+                }
+                if (key.Key == ConsoleKey.LeftArrow || key.Key == ConsoleKey.A)
+                {
+                    // TODO: Move current figure left
+                    CurrentFigureCol--; // TODO: Out of range check
+                }
+                if (key.Key == ConsoleKey.RightArrow || key.Key == ConsoleKey.D)
+                {
+                    // TODO: Move current figure right
+                    CurrentFigureCol++; // TODO: Out of range check
 
-        for (int i = 0; i < TetrisRows; i++) 
-        {
-            string middleLIne = "║";
-            middleLIne += new string(' ', TetrisCols);
-            middleLIne += "║";
-            middleLIne += new string(' ', InfoCols);
-            middleLIne += "║";
-            Console.Write(middleLIne);
+                }
+                if (key.Key == ConsoleKey.DownArrow || key.Key == ConsoleKey.S)
+                {
+                    Frame = 1;
+                    Score++;
+                    CurrentFigureRow++;
+                    // TODO: Move current figure down
+                }
+                if (key.Key == ConsoleKey.Spacebar || key.Key == ConsoleKey.UpArrow)
+                {
+                    // TODO: Implement 90-degree rotation of the current figure
+                }
+
+            }
+                // Update game state
+                if (Frame % FramesToMoveFigure == 0)
+                {
+                    CurrentFigureRow++;
+                    Frame = 0;
+                    Score++;
+
+                }
+
+                /*if (Collision())
+                {
+                    AddCurrentFigureToTetrisField();
+                    CheckForFullLines();
+                    if(lines remove) Score++;
+                }
+                */
+
+                // redraw UI
+                // TODO: DrawTetrisField();
+
+                DrawBorder(); DrawInfo();
+                DrawCurrentFigure();
+
+                Frame++;
+                Thread.Sleep(40);
+            }
         }
 
-        string endLine = "╚";
-        endLine += new string('═', TetrisCols);
-        endLine += "╩";
-        endLine += new string('═', InfoCols);
-        endLine += "╝";
-        Console.Write(endLine);
+
+
+        static void DrawBorder()
+        {
+            Console.SetCursorPosition(0, 0);
+            string line = "╔";
+            line += new string('═', TetrisCols);
+
+            /*for (int i = 0; i < TetrisCols; i++)
+            {
+                line += "═";
+            }*/
+
+            line += "╦";
+            line += new string('═', InfoCols);
+            line += "╗";
+
+            Console.Write(line);
+
+            for (int i = 0; i < TetrisRows; i++)
+            {
+                string middleLIne = "║";
+                middleLIne += new string(' ', TetrisCols);
+                middleLIne += "║";
+                middleLIne += new string(' ', InfoCols);
+                middleLIne += "║";
+                Console.Write(middleLIne);
+            }
+
+            string endLine = "╚";
+            endLine += new string('═', TetrisCols);
+            endLine += "╩";
+            endLine += new string('═', InfoCols);
+            endLine += "╝";
+            Console.Write(endLine);
+
+
+        }
+
+        static void DrawInfo()
+        {
+            Write("Score:", 1, 3 + TetrisCols);
+            Write(Score.ToString(), 2, 3 + TetrisCols);
+            Write("Frame:", 4, 3 + TetrisCols);
+            Write(Frame.ToString(), 5, 3 + TetrisCols);
+        }
+
+        static void DrawCurrentFigure()
+        {
+            var currentFigure = TetrisFigures[CurrentFigureIndex];
+            for (int row = 0; row < currentFigure.GetLength(0); row++)
+            {
+                for (int col = 0; col < currentFigure.GetLength(1); col++)
+                {
+                    if (currentFigure[row, col])
+                    {
+                        Write("█", CurrentFigureRow + row + 1, CurrentFigureCol + col + 1);
+                    }
+                }
+            }
+        }
+
+
+        static void Write(string text, int row, int col, ConsoleColor color = ConsoleColor.Yellow)
+        {
+            Console.ForegroundColor = color;
+            Console.SetCursorPosition(col, row);
+            Console.Write(text);
+            Console.ResetColor();
+        }
 
 
     }
 
-    static void DrawInfo()
-    {
-        Write("Score:", 1, 3 + TetrisCols);
-        Write(Score.ToString(), 2, 3 + TetrisCols);
-
-    }
-
-
-    static void Write(string text, int row, int col, ConsoleColor color = ConsoleColor.Yellow)
-    {
-        Console.ForegroundColor = color;
-        Console.SetCursorPosition(col, row);
-        Console.Write(text);
-        Console.ResetColor();
-    }
-
-
-}
-
-
-
-
+ 
