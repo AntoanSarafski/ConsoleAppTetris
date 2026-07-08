40996d1dd84348b6a9a18d9ce5b449554ee6e0cd
Antoan Sarafski <98612405+AntoanSarafski@users.noreply.github.com>
2026-07-08 16:45:20 +0300

    Add project files.

---
+++ Tetris/Program.cs
@@ -0,0 +1,70 @@
+﻿class Program
+{
+    static int TetrisRows = 20;
+    static int TetrisCols = 10;
+    static int InfoCols = 10;
+    static int ConsoleRows = 1 + TetrisRows + 1;
+    static int ConsoleCols = 1 + TetrisCols + 1 + InfoCols + 1;
+
+
+
+    static void Main(string[] args)
+    {
+        Console.Clear();
+
+        Console.Title = "Tetris v1.0";
+        Console.WindowHeight = ConsoleRows + 1;
+        Console.WindowWidth = ConsoleCols;
+        Console.BufferHeight = ConsoleRows + 1;
+        Console.BufferWidth = ConsoleCols;
+        DrawBorder();
+        Console.ReadLine();
+    }
+
+    static void DrawBorder()
+    {
+        string line = "╔";
+        line += new string('═', TetrisCols);
+
+        /*for (int i = 0; i < TetrisCols; i++)
+        {
+            line += "═";
+        }*/
+
+        line += "╦";
+        line += new string('═', InfoCols);
+        line += "╗";
+
+        Console.Write(line);
+
+        for (int i = 0; i < TetrisRows; i++) 
+        {
+            string middleLIne = "║";
+            middleLIne += new string(' ', TetrisCols);
+            middleLIne += "║";
+            middleLIne += new string(' ', InfoCols);
+            middleLIne += "║";
+            Console.Write(middleLIne);
+        }
+
+        string endLine = "╚";
+        endLine += new string('═', TetrisCols);
+        endLine += "╩";
+        endLine += new string('═', InfoCols);
+        endLine += "╝";
+        Console.Write(endLine);
+
+
+    }
+    static void Write(string text, int row, int col, ConsoleColor color = ConsoleColor.Yellow)
+    {
+        Console.ForegroundColor = color;
+        Console.SetCursorPosition(col, row);
+        Console.WriteLine("Hello, " + text);
+        Console.ResetColor();
+    }
+}
+
+
+
+
