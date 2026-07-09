6fcb355a78d8d8df482661e77b38ceb713f8f176
Antoan Sarafski <98612405+AntoanSarafski@users.noreply.github.com>
2026-07-09 12:58:12 +0300

    Commit with drawing figure UI

--- Tetris/Program.cs
+++ Tetris/Program.cs
@@ -51,24 +51,23 @@ class Program
     static int Score = 0;
     static int Frame = 0;
     static int FramesToMoveFigure = 15;
-    static bool[,] CurrentFigure = TetrisFigures[2]; // Must be random
+    static bool[,] CurrentFigure = null;
     static int CurrentFigureRow = 0;
     static int CurrentFigureCol = 0;
     static bool[,] TetrisField = new bool[TetrisRows, TetrisCols];
+    static Random Random = new Random();
 
 
     static void Main(string[] args)
     {
-        Console.Clear();
-
         Console.Title = "Tetris v1.0";
+        Console.CursorVisible = false;
         Console.WindowHeight = ConsoleRows + 1;
         Console.WindowWidth = ConsoleCols;
         Console.BufferHeight = ConsoleRows + 1;
         Console.BufferWidth = ConsoleCols;
-        Console.CursorVisible = false;
-        DrawBorder();
-        DrawInfo();
+        CurrentFigure = TetrisFigures[Random.Next(0, TetrisFigures.Count)];
+
         while (true)
         {
             Frame++;
@@ -82,17 +81,17 @@ class Program
                 }
                 if (key.Key == ConsoleKey.LeftArrow || key.Key == ConsoleKey.A)
                 {
-                    
-                    if(CurrentFigureCol > 0)
+
+                    if (CurrentFigureCol > 0)
                     {
-                        CurrentFigureCol--; 
+                        CurrentFigureCol--;
                     }
                 }
                 if (key.Key == ConsoleKey.RightArrow || key.Key == ConsoleKey.D)
                 {
-                    if(CurrentFigureCol < TetrisCols - CurrentFigure.GetLength(1))
+                    if (CurrentFigureCol < TetrisCols - CurrentFigure.GetLength(1))
                     {
-                       CurrentFigureCol++; 
+                        CurrentFigureCol++;
                     }
 
                 }
@@ -102,7 +101,7 @@ class Program
                     Frame = 1;
                     Score++;
                     CurrentFigureRow++;
-                    
+
                 }
                 if (key.Key == ConsoleKey.Spacebar || key.Key == ConsoleKey.UpArrow)
                 {
@@ -110,109 +109,162 @@ class Program
                 }
 
             }
-                // Update game state
-                if (Frame % FramesToMoveFigure == 0)
-                {
-                    CurrentFigureRow++;
-                    Frame = 0;
-
-                }
-
-                /*if (Collision())
-                {
-                    AddCurrentFigureToTetrisField();
-                    CheckForFullLines();
-                    if(lines remove) Score++;
-                }
-                */
-
-                // redraw UI
-                // TODO: DrawTetrisField();
-
-                DrawBorder(); DrawInfo();
-                DrawCurrentFigure();
-
-                Thread.Sleep(40);
-            }
-        }
-
-
-
-        static void DrawBorder()
-        {
-            Console.SetCursorPosition(0, 0);
-            string line = "╔";
-            line += new string('═', TetrisCols);
-
-            /*for (int i = 0; i < TetrisCols; i++)
+            // Update game state
+            if (Frame % FramesToMoveFigure == 0)
             {
-                line += "═";
-            }*/
+                CurrentFigureRow++;
+                Frame = 0;
 
-            line += "╦";
-            line += new string('═', InfoCols);
-            line += "╗";
-
-            Console.Write(line);
-
-            for (int i = 0; i < TetrisRows; i++)
-            {
-                string middleLIne = "║";
-                middleLIne += new string(' ', TetrisCols);
-                middleLIne += "║";
-                middleLIne += new string(' ', InfoCols);
-                middleLIne += "║";
-                Console.Write(middleLIne);
             }
 
-            string endLine = "╚";
-            endLine += new string('═', TetrisCols);
-            endLine += "╩";
-            endLine += new string('═', InfoCols);
-            endLine += "╝";
-            Console.Write(endLine);
+            if (Collision())
+            {
+                AddCurrentFigureToTetrisField();
+
+                //CheckForFullLines();
+                //if(lines remove) Score++;
+
+                CurrentFigure = TetrisFigures[Random.Next(0, TetrisFigures.Count)];
+                CurrentFigureRow = 0;
+                CurrentFigureCol = 0;
+            }
 
 
+            // redraw UI
+
+            DrawBorder(); DrawInfo();
+            DrawCurrentFigure();
+
+            DrawTetrisField();
+            Thread.Sleep(40);
+        }
+    }
+
+
+
+    private static void AddCurrentFigureToTetrisField()
+    {
+        for (int row = 0; row < CurrentFigure.GetLength(0); row++)
+        {
+            for (int col = 0; col < CurrentFigure.GetLength(1); col++)
+            {
+                if (CurrentFigure[row, col])
+                {
+                    TetrisField[CurrentFigureRow + row, CurrentFigureCol + col] = true;
+                }
+            }
+        }
+        //CurrentFigure
+        //TetrisField
+    }
+
+    static bool Collision()
+    {
+        // TODO: Collide with existing figure
+        if (CurrentFigureRow + CurrentFigure.GetLength(0) == TetrisRows)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    static void DrawBorder()
+    {
+        Console.SetCursorPosition(0, 0);
+        string line = "╔";
+        line += new string('═', TetrisCols);
+
+        /*for (int i = 0; i < TetrisCols; i++)
+        {
+            line += "═";
+        }*/
+
+        line += "╦";
+        line += new string('═', InfoCols);
+        line += "╗";
+
+        Console.Write(line);
+
+        for (int i = 0; i < TetrisRows; i++)
+        {
+            string middleLIne = "║";
+            middleLIne += new string(' ', TetrisCols);
+            middleLIne += "║";
+            middleLIne += new string(' ', InfoCols);
+            middleLIne += "║";
+            Console.Write(middleLIne);
         }
 
-        static void DrawInfo()
-        {
-            Write("Score:", 1, 3 + TetrisCols);
-            Write(Score.ToString(), 2, 3 + TetrisCols);
-            Write("Frame:", 4, 3 + TetrisCols);
-            Write(Frame.ToString(), 5, 3 + TetrisCols);
-            Write("Position:", 7, 3 + TetrisCols);
-            Write($"{CurrentFigureRow}, {CurrentFigureCol}".ToString(), 8, 3 + TetrisCols);
-            Write("Keys:", 10, 3 + TetrisCols);
-            Write(" ^ ".ToString(), 11, 3 + TetrisCols);
-            Write("<v> ".ToString(), 12, 3 + TetrisCols);
+        string endLine = "╚";
+        endLine += new string('═', TetrisCols);
+        endLine += "╩";
+        endLine += new string('═', InfoCols);
+        endLine += "╝";
+        Console.Write(endLine);
 
+
+    }
+
+    static void DrawInfo()
+    {
+        Write("Score:", 1, 3 + TetrisCols);
+        Write(Score.ToString(), 2, 3 + TetrisCols);
+        Write("Frame:", 4, 3 + TetrisCols);
+        Write(Frame.ToString(), 5, 3 + TetrisCols);
+        Write("Position:", 7, 3 + TetrisCols);
+        Write($"{CurrentFigureRow}, {CurrentFigureCol}".ToString(), 8, 3 + TetrisCols);
+        Write("Keys:", 10, 3 + TetrisCols);
+        Write(" ^ ".ToString(), 11, 3 + TetrisCols);
+        Write("<v> ".ToString(), 12, 3 + TetrisCols);
+
+    }
+
+    static void DrawTetrisField()
+    {
+        for (int row = 0; row < TetrisRows; row++)
+        {
+            for (int col = 0; col < TetrisCols; col++)
+            {
+                if (TetrisField[row, col])
+                {
+                    Write("█", row + 1, col + 1);
+
+                }
+                /*else
+                {
+                    Write(" ", row + 1, col + 1);
+                }*/
+            }
+        }
     }
 
     static void DrawCurrentFigure()
+    {
+        for (int row = 0; row < CurrentFigure.GetLength(0); row++)
         {
-            for (int row = 0; row < CurrentFigure.GetLength(0); row++)
+            for (int col = 0; col < CurrentFigure.GetLength(1); col++)
             {
-                for (int col = 0; col < CurrentFigure.GetLength(1); col++)
+                if (CurrentFigure[row, col])
                 {
-                    if (CurrentFigure[row, col])
-                    {
-                        Write("█", CurrentFigureRow + row + 1, CurrentFigureCol + col + 1);
-                    }
+                    Write("█", CurrentFigureRow + row + 1, CurrentFigureCol + col + 1);
                 }
             }
         }
-
-
-        static void Write(string text, int row, int col, ConsoleColor color = ConsoleColor.DarkRed)
-        {
-            Console.ForegroundColor = color;
-            Console.SetCursorPosition(col, row);
-            Console.Write(text);
-            Console.ResetColor();
-        }
-
-
     }
+
+
+    static void Write(string text, int row, int col, ConsoleColor color = ConsoleColor.DarkRed)
+    {
+        Console.ForegroundColor = color;
+        Console.SetCursorPosition(col, row);
+        Console.Write(text);
+        Console.ResetColor();
+    }
+}
+
+
+
+
+
 
  
